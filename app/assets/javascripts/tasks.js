46a2cd7b97d93998b85e7efbfcfb6765d10fa235
Paul Schoen <pschoen325@gmail.com>
2016-11-27 16:13:45 -0500

    git init

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,13 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function(){
+  $('#task_name').focus();
+  $('#myModal .btn-primary').click(function() {
+    $('.edit_task').submit();
+  });
+});
+
+// $(function() {
+//   console.log('Document is ready!', new Date());
+//
+// });
